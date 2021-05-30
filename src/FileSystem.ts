d24ea4c205b69ac27ab3b67c3935b7ee9c877edd
Elan Ruusamäe <glen@pld-linux.org>
2021-05-30 15:45:05 +0300

    Add exists method to FileSystem

--- src/FileSystem.ts
+++ src/FileSystem.ts
@@ -1,6 +1,10 @@
 import * as fs from "fs";
 
 export class FileSystem {
+  public exists(path: string): boolean {
+    return fs.existsSync(path);
+  }
+
   public writeFile(path: string, content: string): void {
     fs.writeFileSync(path, content, "utf8");
   }
