e31625273f2b932681cbb1f46eee916405a7efc7
Elan Ruusamäe <glen@pld-linux.org>
2021-05-30 14:45:50 +0300

    Setup connection to imap server

--- src/Command/DownloadCommand.ts
+++ src/Command/DownloadCommand.ts
@@ -1,4 +1,6 @@
+import imaps from "imap-simple";
 import { BaseCommand } from "./BaseCommand";
+import { IMAP_HOSTNAME, IMAP_PASSWORD, IMAP_PORT, IMAP_USERNAME } from "../config";
 
 export class DownloadCommand extends BaseCommand {
   protected configure() {
@@ -8,5 +10,21 @@ export class DownloadCommand extends BaseCommand {
   }
 
   protected async execute(): Promise<void> {
+    const connection = await this.getConnection();
+  }
+
+  private async getConnection() {
+    const config = {
+      imap: {
+        user: IMAP_USERNAME,
+        password: IMAP_PASSWORD,
+        host: IMAP_HOSTNAME,
+        port: IMAP_PORT,
+        tls: true,
+        authTimeout: 3000,
+      }
+    };
+
+    return await imaps.connect(config);
   }
 }
